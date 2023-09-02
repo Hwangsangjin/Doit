c9090bd06e91c1fb7acedd9df3b445dcd0c37b8c
Hwangsangjin <hwangsangjin88@gmail.com>
2023-09-03 02:27:36 +0900

    나머지 합 구하기

--- CodingTest/main.cpp
+++ CodingTest/main.cpp
@@ -10,37 +10,58 @@ int main()
 	cin.tie(NULL);
 	cout.tie(NULL);
 
-	// N(배열 크기), Q(질의 수)
-	int N, Q;
-	cin >> N >> Q;
+	// N(수열의 개수), M(나누어떨어져야 하는 수)
+	int N, M;
+	cin >> N >> M;
 
-	// A(원본 배열), D(합 배열)
-	vector<vector<int>> A(N + 1, vector<int>(N + 1, 0));
-	vector<vector<int>> D(N + 1, vector<int>(N + 1, 0));
+	// S(합 배열), C(같은 나머지를 가지는 인덱스를 카운터하는 배열)
+	vector<long> S(N, 0);
+	vector<long> C(M, 0);
 
-	// N만큼 반복
-	for (int i = 1; i <= N; i++)
+	// 정답
+	long Answer = 0;
+
+	// 처음 값 받기
+	cin >> S[0];
+
+	// 1 ~ N
+	for (int i = 1; i < N; i++)
 	{
-		for (int j = 1; j <= N; j++)
-		{
-			// 원본 배열 저장
-			cin >> A[i][j];
+		int Temp = 0;
+		cin >> Temp;
 
-			// 합 배열 저장
-			D[i][j] = D[i][j - 1] + D[i - 1][j] - D[i - 1][j - 1] + A[i][j];
+		// 합 배열 저장
+		S[i] = S[i - 1] + Temp;
+	}
+
+	// 0 ~ N
+	for (int i = 0; i < N; i++)
+	{
+		// 합 배열의 모든 값에 % 연산 수행하기
+		int Remainder = S[i] % M;
+		
+		// 0 ~ i까지의 구간 합 자체가 0일 때 정답에 더하기
+		if (Remainder == 0)
+		{
+			Answer++;
+		}
+
+		// 나머지가 같은 인덱스의 개수 세기
+		C[Remainder]++;
+	}
+
+	// 0 ~ M
+	for (int i = 0; i < M; i++)
+	{
+		// 나머지가 같은 인덱스 중 2개를 뽑는 경우의 수를 더하기
+		if (C[i] > 1)
+		{
+			Answer += C[i] * (C[i] - 1) / 2;
 		}
 	}
 
-	// Q만큼 반복
-	for (int i = 0; i < Q; i++)
-	{
-		int X1, Y1, X2, Y2;
-		cin >> X1 >> Y1 >> X2 >> Y2;
-
-		// 질의 계산 및 출력
-		int Result = D[X2][Y2] - D[X1 - 1][Y2] - D[X2][Y1 - 1] + D[X1 - 1][Y1 - 1];
-		cout << Result << "\n";
-	}
+	// 결과값 출력
+	cout << Answer << "\n";
 
 	return 0;
 }
