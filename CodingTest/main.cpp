841df4b5d63a88d9ca1b75ed55fd98b1b221ad23
Hwangsangjin <hwangsangjin88@gmail.com>
2023-09-02 20:10:57 +0900

    평균 구하기

--- CodingTest/main.cpp
+++ CodingTest/main.cpp
@@ -4,26 +4,40 @@ using namespace std;
 
 int main()
 {
-	// N값 받기
+	// 시험을 본 과목의 갯수
 	int N = 0;
 	cin >> N;
 
-	// 숫자를 string 변수(Numbers)로 입력받기
-	string Numbers;
-	cin >> Numbers;
+	// 과목 데이터 저장 배열
+	int A[1000];
 
-	// Sum 변수 선언
-	int Sum = 0;
-
-	// Numbers 길이만큼 반복
-	for (int i = 0; i < Numbers.length(); i++)
+	// 배열의 길이만큼 반복
+	for (int i = 0; i < N; i++)
 	{
-		// Numbers[i]를 정수로 계산하여 Sum에 누적하기
-		Sum += Numbers[i] - '0';
+		// 점수 저장
+		cin >> A[i];
 	}
 
-	// Sum 출력
-	cout << Sum << "\n";
+	// 총점과 최고점 변수
+	long Sum = 0;
+	long Max = 0;
+
+	// 배열의 길이만큼 반복
+	for (int i = 0; i < N; i++)
+	{
+		// 최고점을 판별하여 저장
+		if (A[i] > Max)
+		{
+			Max = A[i];
+		}
+
+		// 총점 계산
+		Sum += A[i];
+	}
+
+	// 총점 * 100 / 최고점 / 과목수 계산 후 출력
+	double Result = Sum * 100.0 / Max / N;
+	cout << Result << "\n";
 
 	return 0;
 }
